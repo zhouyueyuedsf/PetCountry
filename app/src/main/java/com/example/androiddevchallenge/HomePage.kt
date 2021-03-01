bbcefb913452155a0d0e5d461523085a27c51202
zhouyueyue.joyzhou <zhouyueyue.joyzhou@bytedance.com>
2021-03-01 13:11:52 +0800

    feat: adjust to adapt lint

--- app/src/main/java/com/example/androiddevchallenge/HomePage.kt
+++ app/src/main/java/com/example/androiddevchallenge/HomePage.kt
@@ -3,11 +3,20 @@ package com.example.androiddevchallenge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.*
+import androidx.compose.material.Scaffold
+import androidx.compose.material.TopAppBar
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
@@ -25,9 +34,6 @@ import com.example.androiddevchallenge.data.PetInfo
 fun HomePage(onItemClick: (Long) -> Unit) {
     Scaffold(topBar = {
         TopAppBar(title = { Text(text = "宠物领养") })
-    }, floatingActionButton = {
-        FloatingActionButton(onClick = { }) {
-        }
     }) {
         val petHomePageViewModel: PetHomePageViewModel = viewModel()
         val petInfos by petHomePageViewModel.petInfos.observeAsState()
